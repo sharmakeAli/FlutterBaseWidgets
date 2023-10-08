6f5abf1e3d08432ee90f710fe8462e27bc524664
sharmakekahiye <kahiecode@gmail.com>
2023-10-08 17:45:50 +0300

    image asset

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,8 @@ void main() => runApp(MaterialApp(
           title: Text("App Hello"),
         ),
         body: Center(
-          child: Text("Welcome to Flutter"),
+          child: Image.asset("images/img1.jpg"),
+          // child: Image.network("https://i.ytimg.com/vi/ODci_BNrlfs/maxresdefault.jpg"),
         ),
       ),
     ));
