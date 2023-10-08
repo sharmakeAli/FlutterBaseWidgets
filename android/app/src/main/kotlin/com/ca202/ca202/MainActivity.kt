e95537310c7f7ea4b622cc99c2d53f0d30f2cd2c
sharmakekahiye <kahiecode@gmail.com>
2023-10-08 17:25:04 +0300

    welcome app

---
+++ android/app/src/main/kotlin/com/ca202/ca202/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ca202.ca202
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("App Hello"),
+        ),
+        body: Center(
+          child: Text("Welcome to Flutter"),
+        ),
+      ),
+    ));
