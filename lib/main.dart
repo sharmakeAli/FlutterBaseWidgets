6a209b8fc1eed68f2f6d2db23a10418303af0e5e
sharmakekahiye <kahiecode@gmail.com>
2023-10-13 14:21:21 +0300

    Icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: MyApp(),
-    ));
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    int num = 1;
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Text(num.toString()),
-              TextButton(
-                  onPressed: () {
-                    num++;
-                    print("num is :" + num.toString());
-                    //  print("onPressed");
-                  },
-                  child: Text("Add"))
-            ],
+      home: Scaffold(
+        body: SafeArea(
+            child: Center(
+          child: Icon(
+            Icons.home,
+            color: Colors.blue,
+            size: 40,
           ),
-        ),
+        )),
       ),
-    );
-  }
-}
+    ));
