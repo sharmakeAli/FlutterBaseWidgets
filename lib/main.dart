80c9b52596148398c1d030f9b461287d737396d5
sharmakekahiye <kahiecode@gmail.com>
2023-10-13 14:18:41 +0300

    StatelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,30 @@ import 'MyApp.dart';
 void main() => runApp(MaterialApp(
       home: MyApp(),
     ));
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    int num = 1;
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text(num.toString()),
+              TextButton(
+                  onPressed: () {
+                    num++;
+                    print("num is :" + num.toString());
+                    //  print("onPressed");
+                  },
+                  child: Text("Add"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
